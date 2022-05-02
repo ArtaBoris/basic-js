8ab5d4cc0aa2cda67b9f83338e234184c643f2e9
ArtaBoris <Mister.by4kov2016@yandex.ru>
2022-05-03 02:49:19 +0300

    add two

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,37 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
- * Create transformed array based on the control sequences that original
- * array contains
- * 
- * @param {Array} arr initial array
- * @returns {Array} transformed array
- * 
+ * Extract season from given date and expose the enemy scout!
+ *
+ * @param {Date | FakeDate} date real or fake date
+ * @returns {String} time of the year
+ *
  * @example
- * 
- * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
- * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
+ * getSeason(new Date(2020, 02, 31)) => 'spring'
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  try {
+    if (typeof date === 'undefined') return 'Unable to determine the time of year!';
+    if (isNaN(date)) {
+      throw new Error('Invalid date!');
+    }
+    let month = date.getMonth();
+    let result =
+      month >= 2 && month <= 4
+        ? 'spring'
+        : month >= 5 && month <= 7
+        ? 'summer'
+        : month >= 8 && month <= 10
+        ? 'fall'
+        : 'winter';
+    return result;
+  } catch (error) {
+    throw new Error('Invalid date!');
+  }
 }
 
 module.exports = {
-  transform
+  getSeason,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,51 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+  try {
+    date.toUTCString();
+  } catch {
+    throw new Error("Invalid date!");
+  }
+  if (
+    date instanceof Date &&
+    typeof date.getUTCFullYear() == "number" &&
+    typeof date !== "date" &&
+    !!date.toUTCString()
+  ) {
+    var seasonNumber = date.getMonth();
+  } else {
+    throw new TypeError("Invalid date!");
+  }
+
+  var season;
+
+  if (seasonNumber > 1 && seasonNumber <= 4) {
+    season = "spring";
+  } else if (seasonNumber >= 5 && seasonNumber <= 7) {
+    season = "summer";
+  } else if (seasonNumber >= 8 && seasonNumber <= 10) {
+    season = "autumn";
+  } else {
+    season = "winter";
+  }
+
+  return season;
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
